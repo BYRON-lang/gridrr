2d9ed25734c7480adbfca36ff4d36f07059d0b0a
Byron Pfukwa <byronkennedy22@gmail.com>
2025-07-09 21:09:48 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -13,6 +13,11 @@ root.render(
   </React.StrictMode>
 );
 
+// Show a funny message in the browser console on all pages
+if (typeof window !== 'undefined' && window.console) {
+  console.log('%c🦄 Welcome to the Gridrr dev console! If you see this, you are officially a unicorn hunter. 🦄', 'color: #7f5fff; font-size: 1.2rem; font-weight: bold;');
+}
+
 // If you want to start measuring performance in your app, pass a function
 // 🦄 Nothing to see here, just unicorns debugging!
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
